c49ea4baae1b8b11b0640f031a3cabecb3c8fbdc
sanjay <sanjaysj6282@gmail.com>
2021-06-14 22:54:40 +0530

    Changed a class name in models.py

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models.fields import DateField, SlugField
 
-class Articles(models.Model):
+class Data(models.Model):
     title=models.CharField(max_length=100)
     data=models.TextField()
     time=models.DateTimeField(auto_now_add=True)
